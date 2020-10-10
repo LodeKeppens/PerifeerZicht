b5bcb51c3df8764cec16f16b51e3cdf0b6f24515
Tiranosaurus <lode.keppens@gmail.com>
2020-10-10 11:27:01 +0200

    server, client adjusted for camera transfer

--- client.py
+++ client.py
@@ -1,28 +1,54 @@
 import socket
+import struct
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.26"
+NEW_FRAME_MESSAGE = "!new_frame"
+SERVER = "192.168.56.1"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+    client.send(msg)
 
-send("Hello World!")
-input()
-send("Hello Everyone!")
-input()
-send("Hello Tim!")
+
+    # message = np.char.encode(msg, encoding=FORMAT)
+    # #message = msg.encode(FORMAT)
+    # msg_length = len(message)
+    # send_length = str(msg_length).encode(FORMAT)
+    # print(send_length)
+    # send_length += b' ' * (HEADER - len(send_length))
+    # client.send(send_length)
+    # client.send(message)
+
+while True:
+    _, frame = cap.read()
+    msg_length = client.recv(HEADER).decode(FORMAT)
+    if msg_length:
+        msg_length = int(msg_length)
+        msg = client.recv(msg_length).decode(FORMAT)
+        if msg == NEW_FRAME_MESSAGE:
+            print(frame.shape)
+            if frame is not None:
+                send(struct.pack('921600B', *frame.flat))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 send(DISCONNECT_MESSAGE)
+# send("Hello World!")
+# input()
+# send("Hello Everyone!")
+# input()
+# send("Hello Tim!")
+#
+# send(DISCONNECT_MESSAGE)
--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import socket
+import struct
 import threading
+import cv2
+import numpy as np
 
 HEADER = 64
 PORT = 5050
+SHAPE = (480, 640, 3)
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
@@ -17,18 +21,41 @@ def handle_client(conn, addr):
 
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
+        send(conn, "!new_frame")
 
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
+        data = conn.recv(921600)  # read 230400 bytes
+        arr = np.array(struct.unpack('921600B', data), dtype='B').reshape((640, 480, 3), )
+        cv2.imshow("output", arr)
+
+        # msg_length = conn.recv(HEADER).decode(FORMAT)
+        # if msg_length:
+        #     msg_length = int(msg_length)
+        #     print(msg_length)
+        #     msg = conn.recv(msg_length).decode(FORMAT)
+        #     msg = str2ndarray(msg)
+        #     if msg == DISCONNECT_MESSAGE:
+        #         connected = False
+        #
+        #     print("image")
+        #     cv2.imshow("stream", msg)
+            #print(f"[{addr}] {msg}")
 
     conn.close()
 
+def send(conn, msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    conn.send(send_length)
+    conn.send(message)
+
+def str2ndarray(a):
+    # Specify your data type, mine is numpy float64 type, so I am specifying it as np.float64
+    a = np.fromstring(a, dtype=int)
+    a = np.reshape(a, SHAPE)
+
+    return a
 
 def start():
     server.listen()
