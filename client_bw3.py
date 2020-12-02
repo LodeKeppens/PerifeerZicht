6607c73248efcf51b55db247687b26a8c4809bbf
hek123 <hektor.ltb@gmail.com>
2020-12-02 17:39:34 +0100

    40 fps!!!

--- client_bw3.py
+++ client_bw3.py
@@ -1,6 +1,4 @@
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+from imutils.video import VideoStream
 import cv2
 import time
 import numpy as np
@@ -8,55 +6,34 @@ import socket
 import threading
 from queue import Queue
 import imagezmq
-import matplotlib.pyplot as plt
 
 
-
-def video_stream(q, q_times_video):
+def video_stream(q):
     """
     :param q: queue
     continuously takes pictures and puts them in q
     """
     # initialize the camera
-    camera = PiCamera()
     cam_res = (320, 240)
-    camera.resolution = cam_res
-    camera.framerate = 24
-    rawCapture = PiRGBArray(camera, size=cam_res)
-    time.sleep(0.1)  # allow camera to warm up
+    camera = VideoStream(resolution=cam_res, framerate=40, usePiCamera=True).start()
+    while camera.read() is None:
+        pass
 
     # load transformation matrix, saved on the pi
     M = np.loadtxt('transformation_matrix.csv', delimiter=',')
 
-    # take pictures and transform them
-    times = []
-    time_start = time.time()
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        time_end = time.time()
-        times.append(time_end - time_start)
-        q_times_video.put(times)
-        time_start = time_end
-
-        q.put(cv2.warpPerspective(frame.array, M, (2 * cam_res[0], cam_res[1])))
-        # q.put(frame.array)
-        rawCapture.truncate(0)
+    while True:
+        q.put(cv2.warpPerspective(camera.read(), M, (2 * cam_res[0], cam_res[1])))
 
 
-def stream_video_to_server(q,q_times_to_server):
+def stream_video_to_server(q):
     """
     :param q: queue
     continuously streams the frames in q to the server
     """
     sender = imagezmq.ImageSender(connect_to=f"tcp://{SERVER}:{PORT}")
     pi_name = socket.gethostname()
-    times = []
-    time_start = time.time()
     while True:
-        time_end = time.time()
-        times.append(time_end - time_start)
-        q_times_to_server.put(times)
-        time_start = time_end
-
         sender.send_image(pi_name, q.get())
 
 
@@ -64,29 +41,17 @@ def start():
 
     # create queue to communicate between threads
     q = Queue(maxsize=1)
-    q_times_video = Queue(maxsize=1)
-    q_times_to_server = Queue(maxsize=1)
 
     # create and start the two threads
-    thread = threading.Thread(target=stream_video_to_server, args=(q, q_times_to_server)) # streams video to server
-    cameraThread = threading.Thread(target=video_stream, args=(q, q_times_video))     # capture video stream
+    thread = threading.Thread(target=stream_video_to_server, args=(q, )) # streams video to server
+    cameraThread = threading.Thread(target=video_stream, args=(q, ))     # capture video stream
     print('start processes')
     cameraThread.start()
     thread.start()
 
-    # wait for the threads to terminate
+    # wait for the threads to terminate, which will never happen (:
     thread.join()
     cameraThread.join()
-
-    tijden = {'video':q_times_video.get(), 'to server':q_times_to_server.get()}
-    for key in tijden:
-        plt.scatter(range(len(tijden[key])), tijden[key], label=key)
-    plt.legend()
-    plt.xlabel("frame")
-    plt.ylabel("tijd")
-    plt.ylim(0, 0.12)
-    # plt.xlim(0, len(x))
-    plt.show()
 
 
 if __name__ == '__main__':
@@ -98,4 +63,3 @@ if __name__ == '__main__':
 
     print("[STARTING] client is starting...")
     start()
-    cv2.destroyAllWindows()
--- server_bw3.py
+++ server_bw3.py
@@ -1,78 +1,77 @@
 import multiprocessing as mp
 import cv2
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+from imutils.video import VideoStream
 import time
 import stitcher_bw1
 import paramiko
 import imagezmq
-
-
-def run_client():
-    """
-    runs the client trough ssh, so the user only needs to run the server
-    """
-
-    # ssh in the client
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    ssh.connect(IP_CLIENT, username="pi", password="qwertyui")
-
-    # run the client file
-    # !make sure filename corresponds with the one on the client!
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("python Documents/client_bw3.py")
+import matplotlib.pyplot as plt
 
 
 def stitch_and_show(q_server, q_client):
-    # start display process (we created an extra process because the resizing is very slow)
-    q_video = mp.Queue(maxsize=1)
-    display = mp.Process(target=show, args=(q_video,))
-    display.start()
+    """
+    stitch the two frames and show it
+    """
 
-    time_start = time.time()
+    # create window where the video will be displayed
+    window_name = "camera"
+    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    # make sure we start recieving frames from client
+    q_client.get()
+
+    start = time.time()
+    t1 = start
     nb_frames = 0
     while True:
+        t0 = t1
+        t1 = time.time()
         nb_frames += 1
+
         # get frames
         frame_client = q_client.get()
         frame_server = q_server.get()
+        t2 = time.time()
 
         # stitch the frames
         stitched = stitcher_bw1.stitch_frame_right_warped((frame_server, frame_client))
-
-        # if display has terminated, break the loop
-        if display.exitcode is not None:
-            break
+        t3 = time.time()
 
         # show image
-        # cv2.imshow('video', stitched)
-        q_video.put(stitched)
+        cv2.imshow(window_name, stitched)
+        t4 = time.time()
+
+        # if 'q' is pressed, break from loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
     # print average time per frame
-    time_end = time.time()
-    print((time_end-time_start)/nb_frames)
-    display.terminate()
-    display.join()
+    end = time.time()
+    print('gemiddeld:', (end-start)/nb_frames)
 
 
 def video_stream(q):
     # camera initialization
-    cam = PiCamera()
-    cam.resolution = cam_res
-    cam.framerate = 24
-    rawCapture = PiRGBArray(cam, size=cam_res)
-    time.sleep(0.1)
+    cam_res = (320, 240)
+    camera = VideoStream(resolution=cam_res, framerate=40, usePiCamera=True).start()
+    # wait for camera to start up
+    while camera.read() is None:
+        pass
 
     # take frame, rotate(camera is upside down), and put it in q
-    for frame2 in cam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        q.put(cv2.rotate(frame2.array, cv2.ROTATE_180))
-        rawCapture.truncate(0)
+    frame = camera.read()
+    while True:
+        prev = frame  # little trick to reduce delay between right and left frame
+        frame = cv2.rotate(camera.read(), cv2.ROTATE_180)
+        q.put(prev)
 
 
 def video_stream_from_client(q):
     """
-    :param q: queue where received frames will come
-    Receives the frames from client
+    :param q: qeueu where recieved frames will come
+    recieves the frames from client
     """
     imageHub = imagezmq.ImageHub()
     while True:
@@ -81,33 +80,10 @@ def video_stream_from_client(q):
         q.put(frame)
 
 
-def show(q):
-    """
-    :param q: queue with the stitched frames
-    Shows the live video
-    """
-
-    # create named window where video will be showed
-    window_name = "camera"
-    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-    # get stitched frame and show it
-    while True:
-        # frame = imutils.resize(q.get(), width=640, height=480)
-        frame = q.get()
-        cv2.imshow(window_name, frame)
-
-        # if 'q' is pressed, break from loop and close window
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-
-
 def start():
     """
-    Create queues to communicate between processes
-    then creates and starts stitch and camera processes
+    create queues to communicate between processes
+    then creates and starts stitch and camera proces
     """
     # create queues
     q_server = mp.Queue(maxsize=1)
@@ -115,30 +91,37 @@ def start():
 
     # create and start processes
     stitchProcess = mp.Process(target=stitch_and_show, args=(q_server, q_client))
-    cameraProcess = mp.Process(target=video_stream, args=(q_server,))
-    clientProcess = mp.Process(target=video_stream_from_client, args=(q_client,))
-    clientProcess.start()
+    cameraProcess = mp.Process(target=video_stream, args=(q_server, ))
+    clietProcess = mp.Process(target=video_stream_from_client, args=(q_client, ))
+    clietProcess.start()
     cameraProcess.start()
     stitchProcess.start()
 
     # wait for stitchProcess to terminate, then terminate the other processes
     stitchProcess.join()
     cameraProcess.terminate()
-    clientProcess.terminate()
+    clietProcess.terminate()
     cameraProcess.join()
-    clientProcess.join()
+    clietProcess.join()
 
 
 if __name__ == '__main__':
 
     # initialize connection
     # IP_CLIENT = "169.254.186.249" #LODE
-    # SERVER = "169.254.186.249"    #LODE
     IP_CLIENT = "169.254.27.179"  # HEKTOR
-    SERVER = "169.254.233.181"    # HEKTOR
+    PORT = 5555
 
-    # run_client() # hier is nog probleempje mee, client runt wel degelijk ,(want krijg pycameraerror bij 2e poging)
-    cam_res = (320, 240)
-    FRAME_LENGTH = cam_res[0] * cam_res[1] * 3 + 163  # 230563
+    # ssh in the client
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    ssh.connect(IP_CLIENT, username="pi", password="qwertyui")
+
+    # run client file
+    ssh.exec_command("python Documents/client_bw3.py")
+
     print("[STARTING] server is starting...")
     start()
+
+    # stop the client
+    ssh.exec_command("sudo pkill python")
